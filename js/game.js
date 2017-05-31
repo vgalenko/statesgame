195a2354d440a3bb3a2a7eb55ae7fd468d0fc611
ohjonah <ohjonah@gmail.com>
2017-05-31 13:48:01 -0700

    added game over text at game completion

--- js/game.js
+++ js/game.js
@@ -26,7 +26,7 @@ var answer;
 function askQuestion() {
   answer = shuffledStates[index];
   var populateAnswer = statesObj[statesVar.indexOf(answer)].statesName;
-  populateHeader.innerHTML = populateAnswer;
+  populateHeader.innerHTML = 'Where is ' + populateAnswer + '?';
 }
 
 for (var i = 0; i < statesVar.length; i++) {
@@ -53,6 +53,7 @@ function verify(answer, clicked) {
       index++;
       removeDiv(answer);
       incrementScore();
+      updateUserScore();
       askQuestion();
     } else {
       console.log('loser - counter at:', index);
@@ -61,9 +62,25 @@ function verify(answer, clicked) {
     }
   } else {
     console.log('finished');
+    gameOverText();
     removeListener();
   }
 }
+
+function gameOverText() {
+  if (index === 10) {
+    populateHeader.innerHTML = 'GAME OVER';
+  }
+}
+
+var userStats = document.getElementById('user-score');
+
+function updateUserScore() {
+  var userObj = JSON.parse(localStorage.savedUser);
+  userStats.innerHTML = userObj.name + userObj.score;
+  // userStats.innerHTML = 'UN: ' + userObj.name + ' &#10003;: ' + userObj.score;
+}
+
 
 // removes div
 function removeDiv(answer) {
